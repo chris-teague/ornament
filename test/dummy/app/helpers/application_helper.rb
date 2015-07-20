f45b04070d8479e4806ef9ef2bfd6899bad46698
dbaines <dbaines@gmail.com>
2015-07-20 16:59:01 +0930

    Updated styling of code show/hide, highlighted untested components on index page

--- test/dummy/app/helpers/application_helper.rb
+++ test/dummy/app/helpers/application_helper.rb
@@ -8,9 +8,9 @@ module ApplicationHelper
   def show_code(code)
     toggle_id = Digest::SHA1.hexdigest([Time.now, rand].join)
     code_view = ""
-    code_view = "<div>"
-    code_view += "  <div class='sg-toggle-code'><span data-toggle-anchor='#{toggle_id}' data-toggle-visible-label='Hide code sample'>View code sample</span></div>"
-    code_view += "  <div data-toggle='#{toggle_id}'><pre class='sg-pre prettyprint'>#{render_source(code)}</pre></div>".html_safe
+    code_view = "<div class='sg-toggle-code'>"
+    code_view += "  <div><span data-toggle-anchor='#{toggle_id}' data-toggle-visible-label='Hide code sample'>View code sample</span></div>"
+    code_view += "  <div class='sg-toggle-code-body' data-toggle='#{toggle_id}'><pre class='sg-pre prettyprint'>#{render_source(code)}</pre></div>".html_safe
     code_view += "</div>"
     code_view.html_safe
   end
